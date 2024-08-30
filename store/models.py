ead61a4a125c513de007b8e920fd021d9e489f5c
m.kundan <kundanmeena100@gmail.com>
2024-08-30 15:14:07 +0530

    empty string  error resolved

--- store/models.py
+++ store/models.py
@@ -8,7 +8,7 @@ class Customer(models.Model):
     email = models.CharField(max_length=200,null=True)
 
     def __str__(self):
-        return self.name
+        return self.name if self.name else "Unnamed"
     
 class Product(models.Model):
     name = models.CharField(max_length=200,null=True)
@@ -17,7 +17,7 @@ class Product(models.Model):
     image = models.ImageField(null=True,blank=True)
 
     def __str__(self):
-        return self.name 
+        return self.name if self.name else "Unnamed" 
      
     @property
     def imageURL(self):
